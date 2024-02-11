b5587566e91e568099b41fb21e4db56c61605d3b
ADNCRICH <p50629.2013x@gmail.com>
2024-02-12 01:02:42 +0700

    Move Oauth function to Oauth.go

--- main.go
+++ main.go
@@ -7,31 +7,13 @@ import (
 	"github.com/gin-gonic/gin"
 
 	"DebTour/docs"
-	"io/ioutil"
-	"net/http"
 
-	"os"
+	// "os"
 
 	swaggerFiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
-	"golang.org/x/oauth2"
-	"golang.org/x/oauth2/google"
-
 	//"github.com/gorilla/pat"
 	//"github.com/gorilla/sessions"
-	"fmt"
-)
-
-var (
-	googleOauthConfig = &oauth2.Config{
-		RedirectURL:  "http://localhost:3000/GoogleCallback",
-		ClientID:     os.Getenv("GOOGLE_CLIENT_ID"),
-		ClientSecret: os.Getenv("GOOGLE_CLINET_SECRET"),
-		Scopes: []string{"https://www.googleapis.com/auth/userinfo.profile",
-			"https://www.googleapis.com/auth/userinfo.email"},
-		Endpoint: google.Endpoint,
-	}
-	oauthStateString = "random"
 )
 
 func SetUpSwagger() {
@@ -49,17 +31,22 @@ func SetupRouter() *gin.Engine {
 	return router
 }
 
+func SetupOauth() {
+	controllers.InitializeOauthenv()
+}
+
 func main() {
 	models.InitDB()
 
 	router := SetupRouter()
 	router.LoadHTMLFiles("index.html")
 	SetUpSwagger()
+	SetupOauth()
 	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
-	router.GET("/", HandleMain)
-	router.GET("/GoogleLogin", HandleGoogleLogin)
-	router.GET("/GoogleCallback", HandleGoogleCallback)
+	// router.GET("/", controllers.HandleMain)
+	// router.GET("/GoogleLogin", controllers.HandleGoogleLogin)
+	// router.GET("/GoogleCallback", controllers.HandleGoogleCallback)
 	v1 := router.Group("/api/v1")
 	{
 		v1.GET("/hello", controllers.HelloWorld)
@@ -81,50 +68,10 @@ func main() {
 		// v1.GET("/", controllers.HandleMain)
 		// v1.GET("/GoogleLogin", controllers.HandleGoogleLogin)
 		// v1.GET("/GoogleCallback", controllers.HandleGoogleCallback)
+		v1.GET("/", controllers.HandleMain)
+		v1.GET("/GoogleLogin", controllers.HandleGoogleLogin)
+		v1.GET("/GoogleCallback", controllers.HandleGoogleCallback)
 	}
 
 	router.Run(":9000")
-}
-
-func HandleMain(c *gin.Context) {
-	c.HTML(http.StatusOK, "index.html", gin.H{})
-}
-
-func HandleGoogleLogin(c *gin.Context) {
-	url := googleOauthConfig.AuthCodeURL(oauthStateString)
-	c.Redirect(http.StatusTemporaryRedirect, url)
-}
-
-func HandleGoogleCallback(c *gin.Context) {
-	state := c.Query("state")
-	if state != oauthStateString {
-		fmt.Printf("invalid oauth state, expected '%s', got '%s'\n", oauthStateString, state)
-		c.Redirect(http.StatusTemporaryRedirect, "/")
-		return
-	}
-
-	code := c.Query("code")
-	token, err := googleOauthConfig.Exchange(oauth2.NoContext, code)
-	if err != nil {
-		fmt.Println("Code exchange failed with '%s'\n", err)
-		c.Redirect(http.StatusTemporaryRedirect, "/")
-		return
-	}
-
-	response, err := http.Get("https://www.googleapis.com/oauth2/v2/userinfo?access_token=" + token.AccessToken)
-	if err != nil {
-		fmt.Println("Failed to fetch user info:", err)
-		c.Redirect(http.StatusTemporaryRedirect, "/")
-		return
-	}
-	defer response.Body.Close()
-
-	contents, err := ioutil.ReadAll(response.Body)
-	if err != nil {
-		fmt.Println("Failed to read response body:", err)
-		c.Redirect(http.StatusTemporaryRedirect, "/")
-		return
-	}
-
-	c.String(http.StatusOK, "Content: %s\n", contents)
 }
