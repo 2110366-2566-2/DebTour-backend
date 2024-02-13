57e6c7ec9cfba0d7eb60cab6f48902101ab60a27
ADNCRICH <p50629.2013x@gmail.com>
2024-02-13 08:45:24 +0700

    Changed Middleware

--- go.mod
+++ go.mod
@@ -10,11 +10,14 @@ require (
 )
 
 require (
-	cloud.google.com/go v0.67.0 // indirect
+	cloud.google.com/go v0.110.2 // indirect
+	cloud.google.com/go/compute v1.20.1 // indirect
+	cloud.google.com/go/compute/metadata v0.2.3 // indirect
 	github.com/KyleBanks/depth v1.2.1 // indirect
 	github.com/bytedance/sonic v1.9.1 // indirect
 	github.com/chenzhuoyu/base64x v0.0.0-20221115062448-fe3a3abad311 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.2 // indirect
+	github.com/gin-contrib/sessions v0.0.5 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-openapi/jsonpointer v0.20.2 // indirect
 	github.com/go-openapi/jsonreference v0.20.4 // indirect
@@ -24,8 +27,14 @@ require (
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.14.0 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
+	github.com/golang/glog v1.1.0 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/google/go-cmp v0.5.9 // indirect
+	github.com/google/s2a-go v0.1.4 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/googleapis/enterprise-certificate-proxy v0.2.3 // indirect
+	github.com/googleapis/gax-go/v2 v2.11.0 // indirect
 	github.com/gorilla/context v1.1.2 // indirect
 	github.com/gorilla/mux v1.8.1 // indirect
 	github.com/gorilla/pat v1.0.2 // indirect
@@ -50,6 +59,8 @@ require (
 	github.com/pelletier/go-toml/v2 v2.0.8 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.11 // indirect
+	github.com/zalando/gin-oauth2 v1.5.5 // indirect
+	go.opencensus.io v0.24.0 // indirect
 	golang.org/x/arch v0.3.0 // indirect
 	golang.org/x/crypto v0.19.0 // indirect
 	golang.org/x/net v0.21.0 // indirect
@@ -58,7 +69,11 @@ require (
 	golang.org/x/sys v0.17.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 	golang.org/x/tools v0.17.0 // indirect
+	google.golang.org/api v0.126.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/genproto v0.0.0-20230530153820-e85fd2cbaebc // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20230530153820-e85fd2cbaebc // indirect
+	google.golang.org/grpc v1.55.0 // indirect
 	google.golang.org/protobuf v1.31.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	gorm.io/driver/postgres v1.5.6 // indirect
--- main.go
+++ main.go
@@ -3,10 +3,21 @@ package main
 import (
 	"DebTour/controllers"
 	"DebTour/models"
+	// "go/token"
 
+	// "github.com/gin-contrib/sessions"
 	"github.com/gin-gonic/gin"
 
 	"DebTour/docs"
+	"flag"
+	"fmt"
+	"net/http"
+	"os"
+	"path"
+
+	"github.com/gin-contrib/sessions"
+	"github.com/zalando/gin-oauth2/google"
+	goauth "google.golang.org/api/oauth2/v2"
 
 	// "os"
 
@@ -27,21 +38,54 @@ func SetUpSwagger() {
 
 func SetupRouter() *gin.Engine {
 	router := gin.Default()
-
 	return router
 }
 
-func SetupOauth() {
-	controllers.InitializeOauthenv()
+// func SetupOauth() {
+// 	controllers.InitializeOauthenv()
+// }
+
+var redirectURL, credFile string // new
+func init() {
+	bin := path.Base(os.Args[0])
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, `
+Usage of %s
+================
+`, bin)
+		flag.PrintDefaults()
+	}
+	flag.StringVar(&redirectURL, "redirect", "http://localhost:9000/api/v1/auth/", "URL to be redirected to after authorization.")
+	flag.StringVar(&credFile, "cred-file", "./test-clientid.google.json", "Credential JSON file")
 }
 
+var secret []byte
+var sessionName string
+var stateKey string
+var sessionID string
+
 func main() {
+	flag.Parse()
+	scopes := []string{
+		"https://www.googleapis.com/auth/userinfo.profile",
+		"https://www.googleapis.com/auth/userinfo.email",
+		// You have to select your own scope from here -> https://developers.google.com/identity/protocols/googlescopes#google_sign-in
+	}
+	stateKey = "state"
+	sessionID = "ginoauth_google_session"
+	secret = []byte("secret")
+	sessionName = "goquestsession"
+	google.Setup(redirectURL, credFile, scopes, secret)
+
 	models.InitDB()
 
 	router := SetupRouter()
 	router.LoadHTMLFiles("index.html")
 	SetUpSwagger()
-	SetupOauth()
+
+	router.Use(google.Session(sessionName)) // new
+
+	// SetupOauth()
 	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
 	// router.GET("/", controllers.HandleMain)
@@ -68,10 +112,41 @@ func main() {
 		// v1.GET("/", controllers.HandleMain)
 		// v1.GET("/GoogleLogin", controllers.HandleGoogleLogin)
 		// v1.GET("/GoogleCallback", controllers.HandleGoogleCallback)
-		v1.GET("/", controllers.HandleMain)
-		v1.GET("/GoogleLogin", controllers.HandleGoogleLogin)
-		v1.GET("/GoogleCallback", controllers.HandleGoogleCallback)
+		// v1.GET("/", controllers.HandleMain)
+		// v1.GET("/GoogleLogin", controllers.HandleGoogleLogin)
+		// v1.GET("/GoogleCallback", controllers.HandleGoogleCallback)
+
+		v1.GET("/login", google.LoginHandler)
+		v1.Use(google.Auth())
+		v1.GET("/auth", UserInfoHandler)
+		v1.GET("/api", func(ctx *gin.Context) {
+			ctx.JSON(200, gin.H{"message": "Hello from private for groups"})
+		})
 	}
 
 	router.Run(":9000")
+}
+
+func UserInfoHandler(ctx *gin.Context) { // new
+	var (
+		res goauth.Userinfo
+		ok  bool
+	)
+
+	val := ctx.MustGet("user")
+	if res, ok = val.(goauth.Userinfo); !ok {
+		res = goauth.Userinfo{Name: "no user"}
+	}
+
+	// ctx.JSON(http.StatusOK, gin.H{"Hello": "from private", "user": res.Email})
+
+	output := make(map[string]interface{})
+	output["username"] = res.Id
+	output["email"] = res.Email
+	output["firstname"] = res.GivenName
+	output["lastname"] = res.FamilyName
+	output["picture"] = res.Picture
+
+
+	ctx.JSON(http.StatusOK, gin.H{"success": true, "data": output})
 }
