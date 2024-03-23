9bdc2c118a1932055e2f1bfbfc78d19d697abfb5
Phumipat Chaiprasertsud <phumipat.c@outlook.com>
2024-03-23 19:06:22 +0700

    Clean code, remove unused or commented code

--- controllers/jwtauth.go
+++ controllers/jwtauth.go
@@ -1,15 +1,11 @@
 package controllers
 
 import (
-	"DebTour/database"
-	"DebTour/models"
 	"fmt"
-	"net/http"
 	"os"
 	"time"
 
 	"github.com/dgrijalva/jwt-go"
-	"github.com/gin-gonic/gin"
 )
 
 // JWTService interface
@@ -76,49 +72,5 @@ func (controllers *jwtServices) ValidateToken(encodedToken string) (*jwt.Token, 
 		}
 		return []byte(controllers.secretKey), nil
 	})
-
-}
-
-func ValidateTokenHandler(c *gin.Context) {
-	encodedToken := c.Param("token")
-	token, err := JWTAuthService().ValidateToken(encodedToken)
-
-	if !token.Valid {
-		c.JSON(http.StatusUnauthorized, gin.H{"success": false, "error": err.Error()})
-	} else {
-		claims := token.Claims.(jwt.MapClaims)
-		if claims.Valid() != nil {
-			c.JSON(http.StatusUnauthorized, gin.H{"success": false, "error": "Invalid token"})
-			return
-		}
-		c.JSON(http.StatusOK, gin.H{"success": true})
-	}
-}
-
-func ValidateRoleHandler(c *gin.Context) {
-	encodedToken := c.Param("token")
-	token, err := JWTAuthService().ValidateToken(encodedToken)
-
-	if !token.Valid {
-		c.JSON(http.StatusUnauthorized, gin.H{"success": false, "error": err.Error()})
-		return
-	}
-	claims := token.Claims.(jwt.MapClaims)
-	var user models.User
-	user, err = database.GetUserByUsername(claims["username"].(string), database.MainDB)
-	// check role
-	if err != nil || user.Role != claims["role"] {
-		c.JSON(http.StatusUnauthorized, gin.H{"success": false, "error": "Invalid role"})
-		return
-	}
-	roles := []string{"admin", "user"}
-
-	for _, role := range roles {
-		if role == user.Role {
-			c.JSON(http.StatusOK, gin.H{"success": true})
-			return
-		}
-	}
-	c.JSON(http.StatusUnauthorized, gin.H{"success": false, "error": "mismatch role"})
 
 }
