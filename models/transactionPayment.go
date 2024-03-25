23035723f0639f98540a41f99a058be20bd1612b
Phumipat Chaiprasertsud <phumipat.c@outlook.com>
2024-03-25 15:14:49 +0700

    Models converter, request & response model

--- models/transactionPayment.go
+++ models/transactionPayment.go
@@ -8,3 +8,30 @@ type TransactionPayment struct {
 	TouristUsername string    `gorm:"foreignKey;not null" json:"touristUsername"`
 	Timestamp       time.Time `gorm:"autoCreateTime" json:"timestamp"`
 }
+
+type TransactionPaymentCreateForm struct {
+	TourId          uint   `json:"tourId"`
+	TouristUsername string `json:"touristUsername"`
+	Amount          float64   `json:"amount"`
+	Method          string `json:"method"`
+	Status          string `json:"status"`
+}
+
+func ToTransactionPayment(transaction Transaction, form TransactionPaymentCreateForm) TransactionPayment {
+	return TransactionPayment{
+		TransactionId:   transaction.TransactionId,
+		TourId:          form.TourId,
+		TouristUsername: form.TouristUsername,
+		Timestamp:       time.Now(),
+	}
+}
+
+type FullTransactionPayment struct {
+	TransactionId   uint      `gorm:"foreignKey;not null" json:"transactionId"`
+	TourId          uint      `gorm:"foreignKey;not null" json:"tourId"`
+	TouristUsername string    `gorm:"foreignKey;not null" json:"touristUsername"`
+	Timestamp       time.Time `gorm:"autoCreateTime" json:"timestamp"`
+	Amount          float64   `json:"amount"`
+	Method          string    `json:"method"`
+	Status          string    `json:"status"`
+}
